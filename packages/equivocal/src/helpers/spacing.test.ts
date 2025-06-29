ae8e2f2a7d133bdfd29081a958243decfa25a475
David Hérault <dherault@gmail.com>
2025-06-29 11:08:30 +0300

    Expose failing test

--- packages/equivocal/src/helpers/spacing.test.ts
+++ packages/equivocal/src/helpers/spacing.test.ts
@@ -167,4 +167,53 @@ describe('extractSpacing', () => {
     expect(applySpacing(modifiedCode, spacings)).toEqual(expected)
   })
 
+  test('Extracts and applies spacing on modified code', () => {
+    const code = `
+      function main() {
+        const a = Math.random();
+        const b = Math.random();
+
+        if (a > b) {
+          // This is a comment
+
+          console.log('...');
+
+          // This is another comment
+          console.log('Yes');
+        }
+      }
+    `
+
+    const spacings = extractSpacing(code)
+
+    const modifiedCode = `
+      function main() {
+        const a = Math.random();
+        const b = Math.random();
+        if (a <= b) return;
+        // This is a comment
+        console.log('...');
+        // This is another comment
+        console.log('Yes');
+      }
+    `
+
+    const expected = `
+      function main() {
+        const a = Math.random();
+        const b = Math.random();
+
+        if (a <= b) return;
+        // This is a comment
+
+        console.log('...');
+
+        // This is another comment
+        console.log('Yes');
+      }
+    `
+
+    expect(applySpacing(modifiedCode, spacings)).toEqual(expected)
+  })
+
 })
